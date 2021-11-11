febba888ec88c4587a2ba3d6ea1b44d464a6e950
Emil <emil.varn@gmail.com>
2021-11-11 15:29:42 +0200

    show suggestions and result

--- cmd/web/search.go
+++ cmd/web/search.go
@@ -1,22 +1,93 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
-func searchInsideStruct(artists []ArtistsRelation, userInput string) map[int]string {
+type Suggestions struct {
+	ID             int                 `json:"id"`
+	Image          string              `json:"image"`
+	Name           string              `json:"name"`
+	Members        []string            `json:"members"`
+	CreationDate   int                 `json:"creationDate"`
+	FirstAlbum     string              `json:"firstAlbum"`
+	DatesLocations map[string][]string `json:"relationsSlice"`
+}
+
+var suggestions Suggestions
+
+func searchInsideStruct(artists []ArtistsRelation, userInput string) interface{} {
+
+	isResult := true
+	var returnVal interface{}
+
+	//for _, member := range r.Members {
+	//	if strings.HasPrefix(strings.ToLower(strings.TrimSpace(member)), strings.ToLower(userInput)) {
+	//		m[r.ID] = member + " -> member"
+	//		members := []string{member}
+	//		suggestions.add(members)
+	//	}
+	//}
+
+	// result
+	returnVal, isResult = result(artists, userInput, isResult)
+
+	// suggestion
+	if !isResult {
+		returnVal = suggestion(artists, userInput)
+	}
+
+	//data, _ := json.Marshal(suggestions)
+	//fmt.Println(string(data))
+
+	return returnVal
+}
+
+func result(artists []ArtistsRelation, userInput string, isResult bool) (map[int]string, bool) {
 	m := make(map[int]string)
 
 	for _, r := range artists {
-
-		for _, member := range r.Members {
-			if strings.HasPrefix(strings.ToLower(strings.TrimSpace(member)), strings.ToLower(userInput)) {
-				m[r.ID] = member
+		sliceOfName := strings.Fields(r.Name)
+		for _, name := range sliceOfName {
+			if strings.HasPrefix(strings.ToLower(strings.TrimSpace(name)), strings.ToLower(userInput)) {
+				m[r.ID] = r.Name
 			}
 		}
 
-		if strings.HasPrefix(strings.ToLower(strings.TrimSpace(r.Name)), strings.ToLower(userInput)) {
-			m[r.ID] = r.Name
+		for location := range r.DatesLocations {
+			correctLocations := strings.ReplaceAll(strings.ReplaceAll(location, "_", " "), "-", " - ")
+			if correctLocations == userInput {
+				m[r.ID] = r.Name
+			}
 		}
 
 	}
+
+	if len(m) == 0 {
+		isResult = false
+	}
+
+	return m, isResult
+}
+
+func suggestion(artists []ArtistsRelation, userInput string) []string {
+	var m []string
+
+	for _, r := range artists {
+		for location := range r.DatesLocations {
+			correctLocations := strings.ReplaceAll(strings.ReplaceAll(location, "_", " "), "-", " - ")
+			sliceOfLocations := strings.Fields(correctLocations)
+
+			for _, correct := range sliceOfLocations {
+				if strings.HasPrefix(strings.ToLower(strings.TrimSpace(correct)), strings.ToLower(userInput)) {
+					m = append(m, correctLocations)
+				}
+			}
+		}
+	}
 	return m
+}
+
+func (sugg *Suggestions) add(data []string) {
+	sugg.Members = data
 }
--- ui/fileServer/js/main.js
+++ ui/fileServer/js/main.js
@@ -9,7 +9,7 @@ inputBox.onkeyup = async (e) => {
         await fetch('/search?a=' + userData)
             .then(function (response) {
                 // The response is a Response instance.
-                // You parse the data into a useable format using `.json()`
+                // You parse the data into a usable format using `.json()`
                 return response.json();
             }).then(function (data) {
                 suggestions(data)
@@ -22,21 +22,52 @@ inputBox.onkeyup = async (e) => {
 }
 
 function suggestions(data) {
-
     suggBox.innerHTML = ""
 
-    for (const [key, value] of Object.entries(data)) {
-        let listItem = document.createElement('li');
-        listItem.setAttribute("onclick", "select(" + key + ")");
-        listItem.innerHTML = value;
-        suggBox.appendChild(listItem);
-    }
-    searchWrapper.classList.add("active")
-    let allList = suggBox.querySelectorAll("li");
 
+    let arr;
+    if (!Array.isArray(data)) {
+        for (const [key, value] of Object.entries(data)) {
+            let listItem = document.createElement('li');
+            listItem.setAttribute("onclick", "result(" + key + ")");
+            listItem.innerHTML = value;
+            suggBox.appendChild(listItem);
+        }
+    } else {
+        // Did you mean: text
+        let p = document.createElement('p')
+        p.innerText = "Did you mean: "
+        p.className = "didMean"
+        suggBox.appendChild(p)
+
+        arr = []
+        data.forEach((value, i) => {
+            let listItem = document.createElement('li');
+            listItem.setAttribute("onclick", `suggestion("` + value + `")`);
+            listItem.innerHTML = value;
+
+            // check if suggBox don't have same strings
+            if (!arr.includes(value)) {
+                arr.push(value)
+                suggBox.appendChild(listItem);
+            }
+        })
+    }
+
+    searchWrapper.classList.add("active")
 }
 
-function select(id) {
+function result(id) {
     window.location.href = "http://localhost:8080/artist?id=" + id;
 }
+
+function suggestion(x) {
+    inputBox.value = x;
+
+    inputBox.dispatchEvent(new KeyboardEvent('keyup', {
+        'key': 'a'
+    }));
+}
+
+
 
